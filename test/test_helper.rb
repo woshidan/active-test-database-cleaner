68b4bca8c7e414f99bd746e4572f3ddf7d75d711
woshidan <bibro.pcg@gmail.com>
2015-04-10 00:50:40 +0900

    add lines for config print

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-
-  # Add more helper methods to be used by all tests here...
+  puts "use_transactional_fixtures? " + use_transactional_fixtures?.to_s
+  puts "use_instantiated_fixtures? " + use_instantiated_fixtures?.to_s
+  puts "config " + config.to_s
+  puts "config? " + config?.to_s
+  puts "uses_transaction " + uses_transaction.to_s
 end
