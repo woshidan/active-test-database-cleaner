f1a9d3c38ec6dad758e3770a03327ac96819e4ca
woshidan <bibro.pcg@gmail.com>
2015-04-10 04:05:26 +0900

    confirm to delete record after each test method running

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -6,4 +6,10 @@ class CompanyTest < ActiveSupport::TestCase
     Company.create(name: "田中証券", address: "東京都", established_at: Time.local(2015, 5, 5))
     assert_equal 1, Company.count, "作成後のレコード件数は1件"
   end
+
+  test "create one record twice" do
+    assert_equal 0, Company.count, "作成前のレコード件数は0件"
+    Company.create(name: "田中証券", address: "東京都", established_at: Time.local(2015, 5, 5))
+    assert_equal 1, Company.count, "作成後のレコード件数は1件"
+  end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -6,4 +6,10 @@ class CustomerTest < ActiveSupport::TestCase
     Customer.create(name: "田中太朗", address: "東京都")
     assert_equal 1, Customer.count, "作成後のレコード件数は1件"
   end
+
+  test "create one record twice" do
+    assert_equal 0, Customer.count, "作成前のレコード件数は0件"
+    Customer.create(name: "田中太朗", address: "東京都")
+    assert_equal 1, Customer.count, "作成後のレコード件数は1件"
+  end
 end
